c354159d63db25cefe2e988ee718912b125bf0c3
zeed3v <felipebrionese@gmail.com>
2022-06-14 06:00:22 -0400

    assignment week two

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,32 +1,90 @@
 import React from 'react';
+import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
+function RenderLeader({ leader }) {
+    return(
+        <Media className="mt-4">
+            <Media left middle>
+                <Media object src={leader.image} alt={leader.name} />
+            </Media>
+            <Media body className="ml-4">
+                <Media heading>{leader.name}</Media>
+                <Media>{leader.designation}</Media>
+                <Media className="mt-3">{leader.description}</Media>
+            </Media>
+        </Media>
+    );
+  }
 
 function About(props) {
+
+    const leaders = props.leaders.map((leader) => {
+        return (
+            <div key={leader.id} className="col-12 m-1">
+                <RenderLeader leader={leader} />
+            </div>
+        );
+    });
+
     return(
         <div className="container">
+            <div className="row">
+                <Breadcrumb>
+                    <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>About Us</BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>About Us</h3>
+                    <hr />
+                </div>                
+            </div>
+            <div className="row row-content">
+                <div className="col-12 col-md-6">
+                    <h2>Our History</h2>
+                    <p>Started in 2010, Ristorante con Fusion quickly established itself as a culinary icon par excellence in Hong Kong. With its unique brand of world fusion cuisine that can be found nowhere else, it enjoys patronage from the A-list clientele in Hong Kong.  Featuring four of the best three-star Michelin chefs in the world, you never know what will arrive on your plate the next time you visit us.</p>
+                    <p>The restaurant traces its humble beginnings to <em>The Frying Pan</em>, a successful chain started by our CEO, Mr. Peter Pan, that featured for the first time the world's best cuisines in a pan.</p>
+                </div>
+                <div className="col-12 col-md-5">
+                    <Card>
+                        <CardHeader className="bg-primary text-white">Facts At a Glance</CardHeader>
+                        <CardBody>
+                            <dl className="row p-1">
+                                <dt className="col-6">Started</dt>
+                                <dd className="col-6">3 Feb. 2013</dd>
+                                <dt className="col-6">Major Stake Holder</dt>
+                                <dd className="col-6">HK Fine Foods Inc.</dd>
+                                <dt className="col-6">Last Year's Turnover</dt>
+                                <dd className="col-6">$1,250,375</dd>
+                                <dt className="col-6">Employees</dt>
+                                <dd className="col-6">40</dd>
+                            </dl>
+                        </CardBody>
+                    </Card>
+                </div>
+                <div className="col-12">
+                    <Card>
+                        <CardBody className="bg-faded">
+                            <blockquote className="blockquote">
+                                <p className="mb-0">You better cut the pizza in four pieces because
+                                    I'm not hungry enough to eat six.</p>
+                                <footer className="blockquote-footer">Yogi Berra,
+                                <cite title="Source Title">The Wit and Wisdom of Yogi Berra,
+                                    P. Pepe, Diversion Books, 2014</cite>
+                                </footer>
+                            </blockquote>
+                        </CardBody>
+                    </Card>
+                </div>
+            </div>
             <div className="row row-content">
                 <div className="col-12">
-                <h3>Location Information</h3>
+                    <h2>Corporate Leadership</h2>
                 </div>
-                <div className="col-12 col-sm-4 offset-sm-1">
-                        <h5>Our Address</h5>
-                        <address>
-                        121, Clear Water Bay Road<br />
-                        Clear Water Bay, Kowloon<br />
-                        HONG KONG<br />
-                        <i className="fa fa-phone"></i>: +852 1234 5678<br />
-                        <i className="fa fa-fax"></i>: +852 8765 4321<br />
-                        <i className="fa fa-envelope"></i>: <a href="mailto:confusion@food.net">confusion@food.net</a>
-                        </address>
-                </div>
-                <div className="col-12 col-sm-6 offset-sm-1">
-                    <h5>Map of our Location</h5>
-                </div>
-                <div className="col-12 col-sm-11 offset-sm-1">
-                    <div className="btn-group" role="group">
-                        <a role="button" className="btn btn-primary" href="tel:+85212345678"><i className="fa fa-phone"></i> Call</a>
-                        <a role="button" className="btn btn-info"><i className="fa fa-skype"></i> Skype</a>
-                        <a role="button" className="btn btn-success" href="mailto:confusion@food.net"><i className="fa fa-envelope-o"></i> Email</a>
-                    </div>
+                <div className="col-12">
+                    <Media list>
+                        {leaders}
+                    </Media>
                 </div>
             </div>
         </div>
