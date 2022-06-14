bade19f51d784b21376cc5123a7c361615947c1d
zeed3v <felipebrionese@gmail.com>
2022-06-14 04:45:30 -0400

    single page applications, part two

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
     function RenderDish({dish}) {
-      if (dish != null) {
           return(
               <div className='col-12 col-md-5 m-1'>
                   <Card>
@@ -13,10 +13,10 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
                       </CardBody>
                   </Card>
               </div>
-          )
+          );
           
       }
-    }
+    
 
     function RenderComments({comments}) {
       if (comments != null) {
@@ -27,8 +27,8 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
                 {comments.map((comment) => {
                   return (
                     <li key={comment.id}>
-                      <p> { comment.comment } </p>
-                      <p>-- { comment.author }, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day:'2-digit'}).format(new Date(Date.parse(comment.date)))} --</p>
+                    <p> { comment.comment } </p>
+                    <p>-- { comment.author }, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day:'2-digit'}).format(new Date(Date.parse(comment.date)))} --</p>
                     </li> 
                   );
                 })}
@@ -47,15 +47,23 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
       if (props.dish != null) {
         return(
             <div className="container">
+                <div className='row'>
+                    <Breadcrumb>
+                      <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
+                      <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className='col-12'>
+                      <h3>{props.dish.name}</h3>
+                      <hr />
+                    </div>
+                </div>
                 <div className="row">
                     <RenderDish dish={props.dish} />
-                    <RenderComments comments={props.dish.comments} />
+                    <RenderComments comments={props.comments} />
                 </div>
             </div>
         );
       }
     }
     
-
-
 export default DishDetail;
