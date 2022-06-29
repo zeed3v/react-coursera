030907ccf2f168ea9b68c84a6f2fe4943644125d
zeed3v <felipebrionese@gmail.com>
2022-06-29 15:14:10 -0400

    fetch from server

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { baseUrl } from '../shared/baseUrl'
 
 function RenderLeader({ leader }) {
     return(
         <Media className="mt-4">
             <Media left middle>
-                <Media object src={leader.image} alt={leader.name} />
+                <Media object src={baseUrl + leader.image} alt={leader.name} />
             </Media>
             <Media body className="ml-4">
                 <Media heading>{leader.name}</Media>
@@ -19,7 +20,7 @@ function RenderLeader({ leader }) {
 
 function About(props) {
 
-    const leaders = props.leaders.map((leader) => {
+    const leaders = props.leaders.leaders.map((leader) => {
         return (
             <div key={leader.id} className="col-12 m-1">
                 <RenderLeader leader={leader} />
@@ -83,7 +84,7 @@ function About(props) {
                 </div>
                 <div className="col-12">
                     <Media list>
-                        {leaders}
+                        {leaders}<RenderLeader />
                     </Media>
                 </div>
             </div>
--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -3,6 +3,7 @@ import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbIte
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -95,7 +96,7 @@ class CommentForm extends Component {
 
     return (
       <Card>
-        <CardImg top src={ dish.image } alt={ dish.name } />
+        <CardImg top src={baseUrl + dish.image } alt={ dish.name } />
         <CardBody>
           <CardTitle>{ dish.name }</CardTitle>
           <CardText>{ dish.description }</CardText>
